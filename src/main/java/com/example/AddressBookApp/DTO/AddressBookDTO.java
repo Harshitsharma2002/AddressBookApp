04be3b1655b2bc76f7a2bd320b097f5f908f55d3
Harshit-sharma-cs21 <hrsharma8171087251@gmail.com>
2025-03-08 14:53:42 +0530

    DTO and Model

---
+++ src/main/java/com/example/AddressBookApp/DTO/AddressBookDTO.java
@@ -0,0 +1,12 @@
+package com.example.AddressBookApp.DTO;
+
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressBookDTO {
+    private Long id;
+    private String name;
+    private String phone;
+}
